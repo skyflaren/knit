462a1ec5c06c9f0eadb4f47e900e969cc5cf46fd
dulldesk <54601393+dulldesk@users.noreply.github.com>
2021-01-16 20:37:43 -0500

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// index.html
 import "./index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
